9add9ce4521c79e31ead1cb52e1ffd8ad7dc3ab3
Simon <markhams@tcd.ie>
2017-01-06 16:49:45 +0000

    Updated AppTheme Gave it a name and icon

--- app/src/main/java/nintendont/mapalarm/activity/MapsActivity.java
+++ app/src/main/java/nintendont/mapalarm/activity/MapsActivity.java
@@ -18,7 +18,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
